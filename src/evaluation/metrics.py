9200738f75761d4e3bc6db001b09a1d38b4428eb
Ranim <ranimbarbouchi1@gmail.com>
2026-09-09 19:50:04 +0100

    fix(metrics): Add comprehensive urllib3 SSL bypass for Gemini REST
    Added complete urllib3/requests SSL verification bypass to fix remaining
    CERTIFICATE_VERIFY_FAILED errors when using Gemini REST transport.
    
    Changes:
    - Disable urllib3 InsecureRequestWarning
    - Patch urllib3.util.ssl_.DEFAULT_CIPHERS
    - Override ssl._create_default_https_context globally
    
    This ensures all HTTP libraries (httpx, requests, urllib3) used by
    google-generativeai SDK bypass SSL verification when calling Gemini API.

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -34,14 +34,30 @@ import statistics
 import time
 import warnings
 import httpx
+import urllib3
+import requests
 from groq import Groq, RateLimitError
 from src.config.settings import GROQ_API_KEY
 from src.utils.logger import setup_logger
+from requests.adapters import HTTPAdapter
+from urllib3.util import ssl_ as urllib3_ssl
 
 # Disable SSL verification globally to bypass Avast MITM inspection
 os.environ['PYTHONHTTPSVERIFY'] = '0'
 os.environ['GRPC_DEFAULT_SSL_ROOTS_FILE_PATH'] = ''
 ssl._create_default_https_context = ssl._create_unverified_context
+
+# Disable urllib3 SSL warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Patch requests / urllib3 to ignore SSL globally
+urllib3_ssl.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
 logger = setup_logger(__name__)
 
